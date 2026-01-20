f7d2f21debc97a07b5cc4d213587a6497fa801a9
Alexandre Vieira <200545664+avieira-dev@users.noreply.github.com>
2026-01-20 09:02:55 -0300

    refactor(product): improve maintainability by removing wildcard imports

--- backend/src/main/java/com/agribusiness360/backend/model/Product.java
+++ backend/src/main/java/com/agribusiness360/backend/model/Product.java
@@ -2,8 +2,20 @@ package com.agribusiness360.backend.model;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter

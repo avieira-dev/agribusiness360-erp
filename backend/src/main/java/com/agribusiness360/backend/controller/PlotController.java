865467f80a82de935bd1570ac723751475380645
Alexandre Vieira <200545664+avieira-dev@users.noreply.github.com>
2026-03-04 14:25:53 -0300

    refactor(controller): update PlotController to use DTOs

--- backend/src/main/java/com/agribusiness360/backend/controller/PlotController.java
+++ backend/src/main/java/com/agribusiness360/backend/controller/PlotController.java
@@ -1,6 +1,8 @@
 package com.agribusiness360.backend.controller;
 
 import java.util.List;
+import com.agribusiness360.backend.dto.PlotRequestDTO;
+import com.agribusiness360.backend.dto.PlotResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import com.agribusiness360.backend.model.Plot;
 import com.agribusiness360.backend.model.SoilType;
 import com.agribusiness360.backend.service.PlotService;
 
@@ -37,47 +38,39 @@ public class PlotController {
      *  Search all plots
      */
     @GetMapping
-    public ResponseEntity<List<Plot>> getAllPlots() {
-        List<Plot> plots = plotService.getAllPlots();
-
-        return ResponseEntity.ok(plots);
+    public ResponseEntity<List<PlotResponseDTO>> getAllPlots() {
+        return ResponseEntity.ok(plotService.getAllPlots());
     }
 
     /**
      *  Search for a plot by ID
      */
     @GetMapping("/{id}")
-    public ResponseEntity<Plot> getPlotById(@PathVariable Integer id) {
-        Plot plot = plotService.getPlotById(id);
-
-        return ResponseEntity.ok(plot);
+    public ResponseEntity<PlotResponseDTO> getPlotById(@PathVariable Integer id) {
+        return ResponseEntity.ok(plotService.getPlotById(id));
     }
 
     /**
      *  Search plots from a specific property
      */
     @GetMapping("/property/{id}")
-    public ResponseEntity<List<Plot>> getPlotsByPropertyId(@PathVariable Integer id) {
-        List<Plot> plots = plotService.getPlotsByProperty(id);
-
-        return ResponseEntity.ok(plots);
+    public ResponseEntity<List<PlotResponseDTO>> getPlotsByPropertyId(@PathVariable Integer id) {
+        return ResponseEntity.ok(plotService.getPlotsByProperty(id));
     }
 
     /**
      *  Search plots by name
      */
     @GetMapping("/search-name")
-    public ResponseEntity<List<Plot>> getPlotsByName(@RequestParam String name) {
-        List<Plot> plots = plotService.getPlotsByName(name);
-
-        return ResponseEntity.ok(plots);
+    public ResponseEntity<List<PlotResponseDTO>> getPlotsByName(@RequestParam String name) {
+        return ResponseEntity.ok(plotService.getPlotsByName(name));
     }
 
     /**
      *  Search for a plot by code
      */
     @GetMapping("/search-code")
-    public ResponseEntity<Plot> getPlotByCode(@RequestParam String code) {
+    public ResponseEntity<PlotResponseDTO> getPlotByCode(@RequestParam String code) {
         return ResponseEntity.ok(plotService.getPlotByUniqueCode(code));
     }
 
@@ -85,18 +78,16 @@ public class PlotController {
      *  Search plots by soil
      */
     @GetMapping("/search-soil")
-    public ResponseEntity<List<Plot>> getPlotsBySoil(@RequestParam SoilType soilType) {
-        List<Plot> plots = plotService.getPlotsBySoilType(soilType);
-
-        return ResponseEntity.ok(plots);
+    public ResponseEntity<List<PlotResponseDTO>> getPlotsBySoil(@RequestParam SoilType soilType) {
+        return ResponseEntity.ok(plotService.getPlotsBySoilType(soilType));
     }
 
     /**
      *  Save a new plot
      */
     @PostMapping
-    public ResponseEntity<Plot> savePlot(@RequestBody Plot plot) {
-        Plot newPlot = plotService.savePlot(plot);
+    public ResponseEntity<PlotResponseDTO> savePlot(@RequestBody PlotRequestDTO dto) {
+        PlotResponseDTO newPlot = plotService.savePlot(dto);
 
         return new ResponseEntity<>(newPlot, HttpStatus.CREATED);
     }
@@ -105,8 +96,8 @@ public class PlotController {
      *  Update an existing plot
      */
     @PutMapping("/{id}")
-    public ResponseEntity<Plot> updatePlot(@PathVariable Integer id, @RequestBody Plot plotDetails) {
-        Plot updatedPlot = plotService.updatePlot(id, plotDetails);
+    public ResponseEntity<PlotResponseDTO> updatePlot(@PathVariable Integer id, @RequestBody PlotRequestDTO  dto) {
+        PlotResponseDTO updatedPlot = plotService.updatePlot(id, dto);
 
         return ResponseEntity.ok(updatedPlot);
     }
